e38f8db01011a51d6440f9bb13e6e6b6bc07eca9
Programming-dude1 <deni.ma@live.se>
2021-11-17 11:08:35 +0100

    removed code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use(express.json({extended: false}))
+app.use(express.json({ extended: false }))
 
 const users = [
   {
@@ -20,29 +20,17 @@ app.listen(port, () => {
 /********************************** Get *********************************** */
 app.get("/backend", (req, res) => {
   console.log("User name");
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    res.end(JSON.stringify(users));
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  res.end(JSON.stringify(users));
 });
 
 /*******************************  Post ************************************** */
 
-app.post('/backend', (req, res) =>{
+app.post('/backend', (req, res) => {
   const user = req.body;
   users.push(user)
- res.status(201).send("Created")
+  res.status(201).send("Created")
 
 })
-
-// TODO: fixa post, fel response i clg
-app.post("/backend", (req, res) => {
-  const newperson = [
-    {
-      name: res.send,
-      time: res.send,
-      id: res.send,
-    },
-  ];
-  res.send(newperson);
-});
